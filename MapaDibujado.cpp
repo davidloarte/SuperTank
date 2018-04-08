af2376a2e7911c59abb2497120d6628806d708c1
Guillermo <guille-497@hotmail.com>
2018-04-08 18:25:36 +0200

    Mapa y limites

--- MapaDibujado.cpp
+++ MapaDibujado.cpp
@@ -1,9 +1,11 @@
+
 #include <stdlib.h>
 #include <stdio.h>
 
 #define N 18
 #define M 32 
 
+char tmp_map[N][M];
 char map[N][M] = {
 	"+#############################+",
 	"|                             |",
@@ -25,13 +27,17 @@ char map[N][M] = {
 	"+#############################+"
 	};
 
-void Map()
-{
-	for(int i = 0; i < 18; i++) {
+void Map(){
+	for(int i = 0; i < 18; i++) {			
 		printf("%s\n",map[i] );
 	}
 }
-
+ void limites( int x, int y ){
+	if( tmp_map[y][x] == ' ' || tmp_map[y][x] == '.' ){
+		tmp_map[y][x] = '#';
+	}
+}
+	
 
 int main(){
 
