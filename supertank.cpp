519971a10d158caaf7dbe6b6b1a9456990288044
chiptime <amor95_8@hotmail.com>
2018-04-14 23:13:01 +0200

    tank

--- supertank.cpp
+++ supertank.cpp
@@ -48,7 +48,9 @@ char map[fila][columna] = {
     {2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
     {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2}
 };
-
+int flecha;
+int checky = 0;
+int checkx = 0;
 //Bloques
 
 typedef struct blockUnbreakable {
@@ -67,6 +69,12 @@ typedef struct tanks {
     double y = 0;
 } Tanks;
 Tanks tank;
+typedef struct bullets {
+    double xStart = tank.x;
+    double yStart = tank.y;
+} Bullets;
+
+Bullets bullet;
 
 //Funciones de Curses
 void iniciar_Curses();
@@ -89,10 +97,12 @@ int main() {
     getmaxyx(stdscr,row,col);
 
     while(teclas(&tank.x,&tank.y) != KEY_BREAK){
-     clear();
-     Map();
-     tank1( (int) tank.x, (int) tank.y);
-     usleep(20000);
+        clear();
+        Map();
+        tank1( (int) tank.x, (int) tank.y);
+        teclas(&tank.x,&tank.y);
+
+        usleep(20000);
 
     }
     finalizar_Curses();
@@ -112,40 +122,120 @@ void finalizar_Curses(){
     endwin();
 }
 
+/*bool view(int checker){
+  int count = 0;
+  if (flecha == (258|259|260|261) ){
+  count++;
+  if(count = 2){
+  count = 0;
+  return true;
+  }
+  }
+  else
+  return false;
+  }*/
+/*int direction(double *x, double *y){
+//    int checky = 0;
+//    int checkx = 0;
 
+//    if(checky = 0 || checkx = 0){
+if(*y++)
+checky++;
+if(*y--)
+checky--;
+if(*x++)
+checkx++;
+if(*x--)
+checkx--;
+//    }
+if(checky > 1)
+checky--;
+
+if(checky <(-1))
+checky++;
+
+if(checkx > 1)
+checkx--;
+
+if(checkx < (-1))
+checkx++;
+mvprintw(row-6,0,"checkx es %i, y checky es %i", checkx,checky);
+}*/
 int teclas(double *x, double *y){
 
-    int flecha;
-
+    //    direction(&tank.x, &tank.y);
+    int lastKey = 0;
     flecha = getch();
     //    check_collision();
+    mvprintw(row-5,0,"flecha es %i, y lastkey es %i", flecha, lastKey);
+    mvprintw(row-6,0,"checkx es %i, y checky es %i", checkx,checky);
+
     switch(flecha){
 
         case KEY_UP:
+            //            if(view(flecha) == true){//Check View
+
+            //            }
+            //            else
+            //                changeView();//For change view from Up to Down for example
+             if(checky != 0){
             *y += 1;
             if(*y > ((minf-1) + yInicio ))//Up limit 9
                 *y -= 1;
-            break;
+
+            checkx=0;
+/*            if(checky -= 1)
+                checky=0;*/
+
+            }
+            checky += 1;
+            if(checky > 1)
+                checky -= 1;
+
+
+           break;
 
         case KEY_LEFT:
-            *x += 1;
+           if(checkx != 0){
+           *x += 1;
             if(*x > ((minc-1) + xInicio ))//Left limit 9
                 *x -= 1;
-             break;
+            checky=0;
+           }
+
+            checkx += 1;
+            if(checkx > 1)
+                checkx -= 1;
+            break;
 
         case KEY_DOWN:
+            if(checky != 0){
             *y -= 1;
             if( *y < ((yInicio)+ ( (-maxf)+1 )) )// Down limit -15
                 *y += 1;
+            checkx=0;
+            }
+
+            checky -= 1;
+            if(checky < -1)
+                checky += 1;
             break;
 
         case KEY_RIGHT:
+            if(checkx != 0){
             *x -= 1;
-             if( *x < ((xInicio)+ ( (-maxc)+1 )) )// Right limit -30
+            if( *x < ((xInicio)+ ( (-maxc)+1 )) )// Right limit -30
                 *x += 1;
+            checky=0;
+            }
+            checkx -= 1;
+            if(checkx < -1)
+                checkx += 1;
             break;
     }
-    return flecha;
+    refresh();
+
+    return flecha, checky, checkx;
 }
 
 void tank1(int x, int y) {
@@ -156,14 +246,16 @@ void tank1(int x, int y) {
         mvprintw(row-2,0,"This screen has %d rows and %d columns\n",row,col);
         mvprintw(row-1,0,"errah, %i,%i no es una direccio valida\n", yInicio - y, xInicio - x);
         /*
-        fprintf(stderr, "errah, %i,%i no es una direccio valida\n", yInicio - y, xInicio - x);
-        if((xInicio - x)<0)
-              tank1(x+1, y);
-        if((yInicio - y)<0)
-            tank1(x, y+1);*/
+           fprintf(stderr, "errah, %i,%i no es una direccio valida\n", yInicio - y, xInicio - x);
+           if((xInicio - x)<0)
+           tank1(x+1, y);
+           if((yInicio - y)<0)
+           tank1(x, y+1);*/
     }
     mvprintw(row-1,0,"tank 1 está en, %i,%i", yInicio - y, xInicio - x);
     mvprintw(row-2,0,"tank 1 está en, %i,%i", y, x);
+    mvprintw(row-3,0,"bala está en, %i,%i", bullet.yStart, bullet.xStart);
+
     refresh();
 }
 
@@ -182,5 +274,5 @@ void Map(){
         }
         printw("\n");
     }
-  refresh();
+    refresh();
 }
